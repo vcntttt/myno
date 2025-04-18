0c9c20fccc7e0761a57f7f85391e1774f78b5c8e
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-18 13:45:04 -0400

    refactor: separacion de tienda y auth

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import Header from "@/components/navigation/header";
 
 const geistSans = Geist({
