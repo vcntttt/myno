3edcf0a78eb90500dd7cd1d23f8671be935d5573
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-18 22:52:17 -0400

    feat: SEO para cada página de producto

--- src/app/(shop)/products/[slug]/page.tsx
+++ src/app/(shop)/products/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductDetails } from "@/components/products/product-details";
+import { ProductDetail } from "@/components/products/product-details";
 import { recommendations } from "@/data/products";
 import { notFound } from "next/navigation";
 
@@ -14,11 +14,44 @@ export async function generateStaticParams() {
   }));
 }
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = await params;
+
+  const product = recommendations.find((product) => product.slug === slug);
+  if (!product) return {};
+
+  return {
+    title: product.name,
+    description: product.description,
+    images: [product.image.src],
+    openGraph: {
+      title: product.name,
+      description: product.description,
+      images: [product.image.src],
+    },
+    twitter: {
+      title: product.name,
+      description: product.description,
+      images: [
+        {
+          alt: product.name,
+          url: product.image.src,
+        },
+      ],
+      card: "summary_large_image",
+    },
+  };
+}
+
 export default async function ProductPage({ params }: Props) {
   const { slug } = await params;
 
   const product = recommendations.find((product) => product.slug === slug);
   if (!product) notFound();
 
-  return <ProductDetails product={product} />;
+  return <ProductDetail product={product} relatedProducts={recommendations} />;
 }
