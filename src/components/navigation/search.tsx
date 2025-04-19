80d84ac6113a331cd17b981252063db2a26aabf5
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-19 15:51:52 -0400

    feat: buscador del header conectado con searchparams

--- src/components/navigation/search.tsx
+++ src/components/navigation/search.tsx
@@ -1,15 +1,34 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import { useQueryState } from "nuqs";
 
 export const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useQueryState("query", {
+    defaultValue: "",
+  });
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      window.location.href = `/search?query=${searchQuery}`;
+    }
+  };
+
   return (
     <div className="items-center hidden md:flex">
       <div className="relative flex items-center w-64 h-9">
-        <Input type="search" placeholder="Buscador" className="h-9 w-full" />
+        <Input
+          type="search"
+          placeholder="Buscador"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="h-9 w-full"
+          onKeyDown={handleKeyDown}
+        />
         <Button size="sm" variant="ghost" className="absolute right-0">
-          <Link href={`/search`}>
+          <Link href={`/search?query=${searchQuery}`}>
             <Search className="w-4 h-4" />
           </Link>
         </Button>
