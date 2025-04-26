28c8537c06a91a02c7a8261feaed97271f32b19c
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-26 16:02:36 -0400

    feat: mostrar correo de usuario logeado

--- src/components/profile/user-info.tsx
+++ src/components/profile/user-info.tsx
@@ -1,6 +1,7 @@
+"use client";
 import {
   Card,
-  CardContent,
+  // CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -8,47 +9,50 @@ import {
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { user } from "@/lib/data/user";
 import Link from "next/link";
+import { useUserStore } from "@/store/user";
+import { redirect } from "next/navigation";
 
 export const UserInfo = () => {
+  const user = useUserStore((state) => state.user);
+  const logout = useUserStore((state) => state.logout);
+  function handleLogout() {
+    logout();
+    redirect("/");
+  }
   return (
     <div className="w-full md:w-1/3">
       <Card>
         <CardHeader className="flex flex-col items-center">
           <Avatar className="h-24 w-24">
             <AvatarImage
-              src="https://i.pinimg.com/736x/de/b9/2f/deb92ffd7466cf5830cafb47afeb9b2f.jpg"
+              // src="https://i.pinimg.com/736x/de/b9/2f/deb92ffd7466cf5830cafb47afeb9b2f.jpg"
               alt="Foto de perfil"
             />
-            <AvatarFallback>VR</AvatarFallback>
+            <AvatarFallback>{user?.email[0]}</AvatarFallback>
           </Avatar>
           <CardTitle className="mt-4">
-            {user.firstName} {user.lastName}
+            {} {}
           </CardTitle>
-          <CardDescription>{user.email}</CardDescription>
+          <CardDescription>{user?.email}</CardDescription>
         </CardHeader>
-        <CardContent>
+        {/* <CardContent>
           <div className="space-y-2">
             <div className="flex justify-between">
               <span className="text-muted-foreground">Miembro desde:</span>
-              <span>{user.since}</span>
+              <span>{}</span>
             </div>
             <div className="flex justify-between">
               <span className="text-muted-foreground">Compras:</span>
-              <span>{user.purchases}</span>
+              <span>{}</span>
             </div>
-            {/* <div className="flex justify-between">
-            <span className="text-muted-foreground">Reseñas:</span>
-            <span>{user.reviews}</span>
-          </div> */}
           </div>
-        </CardContent>
+        </CardContent> */}
         <CardFooter className="flex flex-col gap-2">
           <Button variant="outline" className="w-full" asChild>
             <Link href="/profile/history">Ver Historial de Compras</Link>
           </Button>
-          <Button variant="outline" className="w-full">
+          <Button variant="outline" className="w-full" onClick={handleLogout}>
             Cerrar Sesión
           </Button>
         </CardFooter>
